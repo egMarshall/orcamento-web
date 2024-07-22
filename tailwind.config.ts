49a34b2475c89e305d95bebf1d01e05057cf224b
Érico Marshall <ericogmarshall1@gmail.com>
2024-07-22 10:48:24 -0300

    [FEAT] Create Color and Font palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary-background": "#EFEDE7",
+        "menu-bg": "#4F5D75",
+        "primary-title": "#2D3142",
+        "primary-subtitle": "#BFC0C0"
+      },
     },
   },
   plugins: [],
